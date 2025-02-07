69883feff589b980884f1815f8b46398bfe0b71a
Eliaz458 <kanyoroelias356@gmail.com>
2025-02-07 17:08:14 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,11 +6,11 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const defaultOwner = '923444844060';
+const defaultOwner = '254786182066';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
-const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '' ; // put your number here
+const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '254786182066' ; // put your number here
 
 const ownerlist = ownervb.split(';');
 
@@ -20,8 +20,8 @@ for (let i = 0; i < ownerlist.length; i++) {
 }
 //
 global.botname = process.env.BOTNAME || 'MEGA-AI';
-global.pairingNumber = process.env.BOT_NUMBER || '' ;  // put your number here
-global.SESSION_ID = process.env.SESSION_ID || '' ;  // put your session id here
+global.pairingNumber = process.env.BOT_NUMBER || '254786182066' ;  // put your number here
+global.SESSION_ID = process.env.SESSION_ID || 'yMhx2LIB#GGD8Rt5Kam84kN0QmYmp5eU8Be54G3SfBQaAFnDl3m0' ;  // put your session id here
 
 global.mods = []
 global.prems = []
